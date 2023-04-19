08fe6168c45114db56ea266249169423d5ffacfa
Виктор Виноградов <idalys1357@gmail.com>
2023-04-19 22:06:10 +0300

    learning

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import * as React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [dragState, setDragState] = useState(false)
+    const onDrag = (e: React.DragEvent) => {
+        e.preventDefault();
+        setDragState(true)
+    }
+    const onDragLeave = (e: React.DragEvent) => {
+        e.preventDefault();
+        setDragState(false)
+    }
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const onDrop = (e: React.DragEvent) => {
+        e.preventDefault()
+        setDragState(false)
+        const files = [...e.dataTransfer.files]
+        const formData = new FormData();
+        files.forEach(item => {
+            return formData.append('file', item)
+        })
+        console.log(formData.getAll('file'))
+    }
+
+
+    return (
+        <div className="App">
+            <div className='drag'
+                 onDragLeave={onDragLeave}
+                 onDragOver={onDrag}
+                 onDrop={onDrop}
+            >
+                {!dragState ? 'Перетащите файл сюда' : 'Отпустите файл'}
+            </div>
+        </div>
+    )
 }
 
 export default App
